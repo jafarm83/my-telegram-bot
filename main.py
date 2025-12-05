9461fccda3186d3d16a1fdbb3e835e0a6c2a08dc
jafar mohammadi   |  جعفر محمدی <94802995+jafarm83@users.noreply.github.com>
2025-12-05 21:18:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,12 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 # توکن ربات از Environment Variable
-TOKEN = os.environ.get("BOT_TOKEN")
+TOKEN = os.environ.get("8363277121:AAH4wGsId1uUUucQavaG8uvb31mknRkDT5Q")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("ربات Render با موفقیت روشن شد!")
 
 if __name__ == "__main__":
-    # ساخت اپلیکیشن
     app = ApplicationBuilder().token(TOKEN).build()
-    
-    # اضافه کردن دستور /start
     app.add_handler(CommandHandler("start", start))
-    
-    # اجرای ربات با Polling (روی Render بهترین روش است)
     app.run_polling()
