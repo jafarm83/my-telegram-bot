6ed4201ded5eea9b24b693a2c08183d48740c483
jafar mohammadi   |  جعفر محمدی <94802995+jafarm83@users.noreply.github.com>
2025-12-05 21:22:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# توکن ربات از Environment Variable
-TOKEN = os.environ.get("8363277121:AAH4wGsId1uUUucQavaG8uvb31mknRkDT5Q")
+# توکن ربات از Environment Variable می‌آید
+TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("ربات Render با موفقیت روشن شد!")
+    await update.message.reply_text("ربات Railway با موفقیت روشن شد!")
 
 if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+    app.run_polling()  # بهترین روش برای Railway
